4de97c19c5b50a2f68fb4e8c89eb76a85631a4e2
Garik9402 <101651468+Garik9402@users.noreply.github.com>
2022-07-26 15:31:43 +0300

    nootris add adaptive;))

--- js/static.js
+++ js/static.js
@@ -1,4 +1,8 @@
 const burgerNode = document.querySelector('.burger')
+const drropdownNode = document.querySelector('.m-menu')
+const contentNode = document.querySelector('.wrapper-content')
 burgerNode.addEventListener('click', function() {
     burgerNode.classList.toggle('burger--js-active')
+    drropdownNode.classList.toggle('m-menu--js-toggle-drropdown')
+    contentNode.classList.toggle('wrapper-content--js-toggle')
 })
